be7893238da1784ea2464ca03fddc70bc1eb6bb8
W-Dear <162520602+W-Dear@users.noreply.github.com>
2024-06-27 09:43:34 +0900

    Update myppp.tsx

--- components/component/myppp.tsx
+++ components/component/myppp.tsx
@@ -4,10 +4,6 @@ import { Button } from "@/components/ui/button";
 export function MyPPP() {
   return (
     <>
-      <Head>
-        <title>안진섭의 포트폴리오</title>
-      </Head>
-
       <header className="w-full bg-[#BFB5A2] text-gray-50 py-6 px-4 md:px-6">
         <div className="container max-w-6xl mx-auto flex items-center justify-between">
           <Link className="flex items-center gap-2" href="#">
